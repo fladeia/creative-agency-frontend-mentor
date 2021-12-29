2a02f0e2d8da642aba526dfb1a84fd0d5513c625
fladeia <252842.5s@gmail.com>
2021-12-29 09:10:01 -0300

    added sections wave. todo text

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,8 +6,10 @@ import logo from '../../assets/logo.svg'
 import Hamburger from '../../assets/mobile/icon-hamburger.svg'
 import strategic from  '../../assets/desktop/image-strategic.jpg'
 import slide1 from '../../assets/desktop/image-slide-1.jpg'
-import slide2 from '../../assets/desktop/image-slide-2.jpg'
-import slide3 from '../../assets/desktop/image-slide-3.jpg'
+// import slide2 from '../../assets/desktop/image-slide-2.jpg'
+// import slide3 from '../../assets/desktop/image-slide-3.jpg'
+import waveWhite from '../../assets/desktop/bg-pattern-wavy-white.svg'
+import waveRed from '../../assets/desktop/bg-pattern-wave-red.svg'
 import '../styles/home.css'
 import '../styles/header.css'
 
@@ -55,14 +57,18 @@ export const Home = () => {
       {/* ---------------------------------------------------section */}
       <div className='sections'>
         <div className='section1'>
-          <img src={strategic} alt='illustration 1'></img>
+          <img src={strategic} alt='illustration 1' className='illustration1'></img>
         </div>
-        <div className='section2'>2</div>
+        <div className='section2'>
+          <img src={waveRed} alt='red wave' className='red-wave'></img>
+        </div>
         <div className='section3'>3</div>
         <div className='section4'>4</div>
-        <div className='section5'>5</div>
+        <div className='section5'>
+          <img src={waveWhite} alt='white wave' className='white-wave'></img>
+        </div>
         <div className='section6'>
-          <img src={slide1} alt='illustration 1'></img>
+          <img src={slide1} alt='illustration 1' className='illustration2'></img>
         </div>
       </div>
 
